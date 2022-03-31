ed959071247f9e3ee60d7c1819a88a62abe25625
Jeff Workman <jworkhollr@aol.com>
2022-03-31 16:05:02 -0400

    init commit

---
+++ src/app/customer-list/customer-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../models/customer.class';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css']
+})
+export class CustomerListComponent implements OnInit {
+
+  customers: Customer[] = [
+    new Customer(1, "MAX", "OH"),
+    new Customer(2, "PG", "OH"),
+    new Customer(3, "Target", "MN")
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
